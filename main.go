d6673f006f3b360f4bfb27ade6339333bbe18a56
EC2 Default User <ec2-user@ip-172-31-22-53.ec2.internal>
2020-09-23 08:14:23 +0000

    first commit

--- main.go
+++ main.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// Handler is executed by AWS Lambda in the main function. Once the request
-// is processed, it returns an Amazon API Gateway response object to AWS Lambda
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func Test() (string, error) {
 
-	index, err := ioutil.ReadFile("public/index.html")
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       string(index),
-		Headers: map[string]string{
-			"Content-Type": "text/html",
-		},
-	}, nil
-
+	return fmt.Sprintf("Welcome to AWS_Session"), nil
 }
 
 func main() {
-	lambda.Start(Handler)
+
+	lambda.Start(Test)
 }
